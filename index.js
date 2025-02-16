5d9f733b292898b4925e40aaf88e4664508cd270
ishaq876 <katala007706@gmail.com>
2025-02-16 12:21:13 +0500

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const Crypto = require('crypto')
 const path = require('path')
 const prefix = config.PREFIX
 
-const ownerNumber = ['923003588997']
+const ownerNumber = ['923352215918']
 
 //===================SESSION-AUTH============================
 if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
