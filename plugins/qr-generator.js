a99007e7085e2307e49cc048f7af7998374cbcc7
MIAN RAHEEL <139443261+MIANG-8@users.noreply.github.com>
2025-03-17 17:08:36 +0500

    Update qr-generator.js

--- plugins/qr-generator.js
+++ plugins/qr-generator.js
@@ -38,12 +38,12 @@ cmd({
 }) => {
   try {
     if (!q) return reply('Please provide text to generate QR code.');
-    await reply('> *ali Generating QR code...🔄*');
+    await reply('> *RAHEEL Generating QR code...🔄*');
     const apiUrl = `https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(q)}&size=200x200`;
     const response = await axios.get(apiUrl, { responseType: 'arraybuffer' });
     const buffer = Buffer.from(response.data, 'binary');
     
-    await conn.sendMessage(m.chat, { image: buffer }, { quoted: m, caption: 'QR Code By ALi Md' });
+    await conn.sendMessage(m.chat, { image: buffer }, { quoted: m, caption: 'QR Code By RAHEEL Md' });
   } catch (error) {
     console.error(error);
     reply(`An error occurred: ${error.message}`);
