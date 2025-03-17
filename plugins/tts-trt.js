017e0192ec7e363023cd3cc968c800b847c4bd58
MIAN RAHEEL <139443261+MIANG-8@users.noreply.github.com>
2025-03-17 17:35:16 +0500

    Update tts-trt.js

--- plugins/tts-trt.js
+++ plugins/tts-trt.js
@@ -24,7 +24,7 @@ async (conn, mek, m, { from, q, reply }) => {
         const response = await axios.get(url);
         const translation = response.data.responseData.translatedText;
 
-        const translationMessage = `> *ALI-MD-TRANSLATION*
+        const translationMessage = `> *RAHEEL-MD-TRANSLATION*
 
 > 🔤 *Original*: ${textToTranslate}
 
