0742362fd823b0bffddfcddb13d9ed3672bae2ac
Soumyajit09786 <barnalisasmal813@gmail.com>
2025-08-10 18:06:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -77,7 +77,7 @@ DEV: process.env.DEV || "923003588997",
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
 ANTI_DELETE: process.env.ANTI_DELETE || "true",
-ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "same", 
+ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "inbox", 
 // change it to 'same' if you want to resend deleted message in same chat 
 AUTO_RECORDING: process.env.AUTO_RECORDING || "false"
 // make it true for auto recoding 
