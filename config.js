0ef257574c5d8e7be71d0f6ac6645360c7142432
Soumyajit09786 <barnalisasmal813@gmail.com>
2025-08-17 17:02:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ALI-MD~PF9W0LQA#zMoQMcMMjpPg90-kFjyC0pVbu_ZrUGaRTJRaZ4n5xXs",  
+SESSION_ID: process.env.SESSION_ID || "ALI-MD~H4sIAAAAAAAAA61VO4/bRhD+K8G2EiyS4lPAAaFIUa/TW6QkBilW5PItklou9TIE2EAqB2lSuYibxIADIwhSBAkQ2D/BfS5V0qY5/4SAujv7ADv2BQiLwe5wOfPNt/MN74M48TPURXtQuw9S7G8gQcWS7FMEaqCeOw7CoAxsSCCoAbF9PvVsNNJo8yDTFO5aeYdqJuak7fWDrnUeOcNRbhreVGfPwLEM0nwZ+dYHAh7a+dBIA352kA/ObC3ng2BRXfSaOybyd4ncWy2l9cY4NAStdwaORUToYz92G6mHVgjDqIv2Q+jju8EfqGpshdMSWfWZ7czrTfeB2LVWa4tz88YiFOw0tGlzmgmr0d3g93v2xjogFkWNc2VlRkOpN2gvbYWm6jtqbNr9VGqpU+xwOnUFP/PdGNltG8XEJ/s78x52hO2EMLpCGdtOZ3zIzDlfWs6R6VrigPC6miIvMZoxZWR3A96mFx2n2qKcFs/tS11RlNZM6AUlgWz0XTs0BxWzK9moSanhbeBDfNMr4X/h3a3HM97MmnIp7WU5ToNuZSYoyMSDulTqT8drKIihS8vK7I68x94kd/fyEHfYsb7T53E+3+pxKaBcXxtv/e2QJuEgGIZJfIt3SHL8IZRamyt5iumrSX0xUdv6WEtpmLUcZ1NXjcXYiEPU5fPSqN8atpJWYjJIaxAjjbjt2GTNgMhY1yi2Af2lgTdCX9mzc9nfnp0qCtG+bYMafSwDjFw/IxgSP4kLn1QtA2hvJsjCiJzYBaoYKxhvaG7eC3qy2JKt7URQdj0u5oeUOezHVL7b8/UM5/oZKIMUJxbKMmS3/IwkeN9DWQZdlIHaZ6eLKmrGaJUQ1PFtUAMSLTIcxbGCyFCfZve2HiQZTNN7MSKgDBycrHoI1AjOURmcPmAZVZUpVqUoSmzQqspIlCYwjMzTEqvybLWocHWVdOqvUEbgKgU1WuA4lpEkkTuW/ycctKoJKt0QG7KgybLWEBlGkHiOUmlVYyn1ozg+L4MY7chVGxfkV+kycHycET3O0yiB9k2P37yElpXkMZnsY0spFgiD2i03IsSP3ayoLI8htjx/g5SiDlBzYJShN/eNMLJvarmeYUpiF21YpUfGhGa6oMBeBHqHm1qVe5eeGBanweWLp5cvH16+/Pry5ZeXL776+9nD108e//zJ6yePfyrML4X5sTC/FebbP1988fuzi0d/PL948OqHiwcXD149Lexfv35z2nx/st+d7POLBxePQBlEJzwiw4uMxEuUKDI0WyAq/Mc3TBTAbUSgH2WgBpT28jxN3HqjV2W9TG825YYrK64M3jJ3I8irjteCzVKInHARl/xtNx+16TTRJH0EWSK5Ky23ZsGIR5RuyOzZe4KAGlhWtExrSS0vbFqzDE6SBpS2Xn1t87PDFPmDfkpHLFR2db6yH26RS+/2skAPMNNtTLaVScc1xM6+0mcSkvJkxGhCKerK7lmRzUYb30K3k439XTCcM7ZWyqMurQ6NVqck6eJapqmDEaca0+HjoNsJSoeUh9XBfH1uDElblUbcwFJR0GT1elrxImjhZtjYnE9ZxBqBezUqTqMquv5F+Ncq9k9bx0eniXt99x/tkSvgRStTx/KtGNcz/F/mYH28EJgSN+eanUqSKThU+hDWHaPRzM+dRcdbxlpCDWKiaaMBOBaaSiNInASvQA1kqyUEZYCTvBBGO3aSD2RSZKotX5cdwYzIb8X2Hv0K4tWpIU7SFsy8IgDrdPqFcPZymk4IJDfSBXLxaNYEHP8BFNWdFeQIAAA=",  
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -32,7 +32,7 @@ ALIVE_IMG: process.env.ALIVE_IMG || "https://qu.ax/zrqFX.jpg",
 // add custom menu image url
 PREFIX: process.env.PREFIX || ".", 
 // add your prifix for bot   
-BOT_NAME: process.env.BOT_NAME || "ALI-MD",
+BOT_NAME: process.env.BOT_NAME || "DERK- SOUMYA",
 // add bot namw here for menu
 STICKER_NAME: process.env.STICKER_NAME || "ALI-MD",    
 VPS: process.env.VPS || "",
@@ -72,7 +72,7 @@ AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "923003588997",
+DEV: process.env.DEV || "918250547820",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
